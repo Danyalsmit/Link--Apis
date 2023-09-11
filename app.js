0c70621bf8f4bc87fe0f6d9028ef440665a046c8
danyalsmit <dk818552@gmail.com>
2023-09-11 23:24:09 +0500

    json

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-function foo(){
-    let chan = document.getElementById("hg")
-  
+(async function(){
+const response = await fetch("https://dummyjson.com/products");
+const products = await response.json();
+
+
+const plc = document.getElementById("plc")
+const btn = document.getElementById("btn")
+
+function search(){
+    const query = plc.value;
+    console.log(query);
+
 }
+btn.addEventListener("click",search);
+
+})();
+
+
+            
+
+
+  
