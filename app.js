ce4f3a31645717ffbcda227ea0a4f6f6c0fe93f6
danyalsmit <dk818552@gmail.com>
2023-09-19 22:10:21 +0500

    cardupdated

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
-(async function(){
-const response = await fetch("https://dummyjson.com/products");
-const products = await response.json();
+// fetch('./products.json') // Assuming products.json is in the same directory as your HTML file
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then(data => {
+//         const product = data.products; // Assuming you want to display the first product
+
+//         // Select elements by their IDs
+//         const pic = document.getElementById("pic");
+//         const des = document.getElementById("des");
+//         const tex = document.getElementById("tex");
+
+//         // Update card elements with product data
+//         pic.src = product.thumbnail;
+//         des.textContent = product.title;
+//         tex.textContent = product.description;
+//     })
+//     .catch(error => console.log(error));
+
+fetch('./products.json')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        const products = data.products;
+        const productInfoContainer = document.getElementById('product-info-container');
 
 
-const plc = document.getElementById("plc")
-const btn = document.getElementById("btn")
+        // Iterate over the products array
+        products.forEach(product => {
+            // Access individual product properties here
+            const productId = product.id;
+            const productTitle = product.title;
+            const productDescription = product.description;
+            const productPrice = product.price;
+            const productImages = product.thumbnail;
 
-function search(){
-    const query = plc.value;
-    console.log(query);
+            // ... and so on for other properties
+            const productDiv = document.createElement('div');
+            productDiv.innerHTML = `
+                <p>Product ID: ${productId}</p>
+                <p>Product Title: ${productTitle}</p>
+                <p>Product Description: ${productDescription}</p>
+                <p>Product Price: $${productPrice}</p>
+                <img src="${productImages}" /> 
+                <hr>
+            `;
 
-}
-btn.addEventListener("click",search);
-
-})();
-
-
-            
+            // Append the product information to the container
+            productInfoContainer.appendChild(productDiv);
+        });
+    })
+    .catch(error => console.log(error));
 
 
-  
+
+
+
+
