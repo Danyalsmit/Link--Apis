f43693b9125ff9977f71592edae2032d30d1161c
danyalsmit <dk818552@gmail.com>
2023-09-27 23:09:32 +0500

    addsome data

--- app.js
+++ app.js
@@ -1,81 +1,53 @@
-   
-  //   (async function () {
-  //     fetch('./products.json').then((data) => {
-  //     return data.json();
-  //    })
-  //   .then((products) => {
-  //     console.log(products);
-  //     let data1 = ""; 
-  //     products.products.map((values) => {
-  //         data1 += `
-  //         <div class="card" style="width: 18rem;">
-  //           <img id="pic" src="${values.thumbnail}" height="200px" width="200px" class="card-img-top" alt="...">
-  //           <div class="card-body">
-  //             <h5 class="card-title">${values.title}</h5>
-  //             <p class="card-text">${values.description}</p>
-  //             <p class="card-text">${values.price}</p>
-  //           </div>
-  //         </div>`;
-  //         const get = document.getElementById("container");
-  //         get.innerHTML = data1;
-  //      });
-  // })
-  // .catch((error) => {
-  //     console.log(error);
-  // });
-
-
-    
-  // (async function () {
-  //   const response = await fetch('./products.json');
-  //   const products = await response.json();
+// (async function () {
+//     const response = await fetch('./products.json');
+//     const products = await response.json();
   
-  //   const inp = document.getElementById("input");
-  //   const dis = document.getElementById("display");
+//     const inp = document.getElementById("input");
+//     const dis = document.getElementById("display");
   
   
-  //   function dispResult(results) {
-  //     results.forEach(function (product) { 
-  //       let data1 = "";
-  //       data1 += `
-  //       <div class="card">
-  //         <img id="pic" src="${product.thumbnail}" class="card-img-top" alt="...">
-  //         <div class="card-body">
-  //           <h5 class="card-title">${product.title}</h5>
-  //           <p class="card-text">${product.description}</p>
-  //           <p class="card-text">$ ${product.price}</p>
-  //         </div>
-  //       </div>`;
-  //     });
+//     function dispResult(results) {
+//       results.forEach(function (product) { 
+//         let data1 = "";
+//         data1 += `
+//         <div class="card">
+//           <img id="pic" src="${product.thumbnail}" class="card-img-top" alt="...">
+//           <div class="card-body">
+//             <h5 class="card-title">${product.title}</h5>
+//             <p class="card-text">${product.description}</p>
+//             <p class="card-text">$ ${product.price}</p>
+//           </div>
+//         </div>`;
+//       });
   
-  //     const get = document.getElementById("container");
-  //     get.innerHTML = data1;
-  //   }
+//       const get = document.getElementById("container");
+//       get.innerHTML = data1;
+//     }
   
-  //   function search() {
-  //     const query = inp.value.toLowerCase();
-  //     const results = products.filter(function (product) {
-  //       return product.title.toLowerCase().includes(query);
-  //     });
+//     function search() {
+//       const query = inp.value.toLowerCase();
+//       const results = products.filter(function (product) {
+//         return product.title.toLowerCase().includes(query);
+//       });
   
-  //     dispResult(results); 
-  //   }
+//       dispResult(results); 
+//     }
   
-  //   dis.addEventListener("click", search); 
+//     dis.addEventListener("click", search); 
   
-  //   search();
-  // })().catch(error => {
-  //   console.log(error);
-  // });
+//     search();
+//   })().catch(error => {
+//     console.log(error);
+//   });
 
 
   fetch('./products.json')
-    .then((data) => {
-        return data.json();
+    .then((res) => {
+        return res.json();
     })
     .then((products) => {
         console.log(products);
-        let data1 = ""; // Change const to let
+        let data1 = ""; 
         products.products.map((values) => {
             data1 += `
             <div class="card" style="width: 18rem;">
@@ -90,6 +62,5 @@
             get.innerHTML = data1;
         });
     })
-    .catch((error) => {
-        console.log(error);
-    });
+    
+
